264bfcc801c1e684d178c1df598dd8d5a45b863d
manitu85 <manitu.psy@gmail.com>
2020-06-22 23:54:36 +0200

    #

--- components/hoc/layout.component.jsx
+++ components/hoc/layout.component.jsx
@@ -5,31 +5,22 @@ import {
   Header,
   HeaderLink,
   Nav,
-  Footer } from './layout.styles';
+  Footer
+} from './layout.styles';
 
 
 const Layout = ({ children, title }) => {
   return (
     <Root className="root">
-        <Header>
-          <Nav>
-            <StyledLink href="/">
-              Home
-            </StyledLink>{' '}
-            <StyledLink href="/about">
-              About
-            </StyledLink>{' '}
-            <StyledLink href="/portfolio">
-              Portfolio
-            </StyledLink>{' '}
-            <StyledLink href="/blog">
-              Blog
-            </StyledLink>{' '}
-            <StyledLink href="/contact">
-              Contact
-            </StyledLink>
-          </Nav>
-        </Header>
+      <Header>
+        <Nav>
+          <StyledLink href="/">Home</StyledLink>{' '}
+          <StyledLink href="/about">About</StyledLink>{' '}
+          <StyledLink href="/portfolio">Portfolio</StyledLink>{' '}
+          <StyledLink href="/blog">Blog</StyledLink>{' '}
+          <StyledLink href="/contact">Contact</StyledLink>
+        </Nav>
+      </Header>
       <h1>{title}</h1>
       {children}
       <Footer><p>Copyright&copy; Digital Genetics {new Date().getFullYear()}</p></Footer>
